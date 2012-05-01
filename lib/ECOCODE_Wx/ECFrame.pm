fe2662241cad74c3633c01b29cc4aff58f4a5d8a
Erik Colson <eco@ecocode.net>
2012-05-01 11:34:53 +0200

    Added license and short doc

--- lib/ECOCODE_Wx/ECFrame.pm
+++ lib/ECOCODE_Wx/ECFrame.pm
@@ -1,5 +1,42 @@
 package ECOCODE_Wx::ECFrame;
 
+=head1 NAME
+
+    ECFrame - Moose class around a customized Wx Frame
+
+=head1 SYNOPSIS
+
+
+
+=head1 DESCRIPTION
+
+
+
+=head1 LICENSE
+
+    This file is part of ECOCODE_Wx.
+
+    ECOCODE_Wx is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published
+    by the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    ECOCODE_Wx is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2012 Erik Colson
+
+=head1 AUTHOR
+
+    Erik Colson
+
+=cut
+
 use strict;
 use warnings;
 use feature "switch";
