34d8e40b6c9203fd5e349d0d3c0922b0c63a0010
Erik Colson <eco@ecocode.net>
2014-05-09 15:08:07 +0200

    handle EVT_TEXT on ECDataAwareTextCtrl

--- lib/ECOCODE_Wx/ECDataAwareTextCtrl.pm
+++ lib/ECOCODE_Wx/ECDataAwareTextCtrl.pm
@@ -5,9 +5,36 @@ use Moose;
 extends 'ECOCODE_Wx::ECTextCtrl' ;
 
 use DBIx::Class::Row;
+use Wx qw (wxNullColour);
+use Wx::Event qw( EVT_TEXT );
 
 has 'dbicColumn' => ( is => 'rw', isa => 'Str', );
 has 'currentRow' => ( is => 'rw', isa => 'Maybe[DBIx::Class::Row]', );
+
+sub BUILD {
+    my ($self,$args) = @_;
+    EVT_TEXT ( $self, $self,
+               sub {
+                   my ($this,$event) = @_;
+                   if ($this->isChanged == 1) {
+                       $this->SetBackgroundColour( Wx::Colour->new('yellow'));
+                       $this->Refresh();
+                   } else {
+                       $this->SetBackgroundColour( wxNullColour );
+                       $this->Refresh();
+                   }
+               }
+           );
+}
+
+sub isChanged {
+    my $self = shift;
+    my $curValue = $self->GetValue()||'';
+    return undef if (!$self->currentRow());
+    my $dbValue = $self->currentRow()->get_column($self->dbicColumn)||'';
+    return 1 if ($curValue ne $dbValue);
+    return 0;
+}
 
 sub setToDBICResult {
     my $self = shift;
